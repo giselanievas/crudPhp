979b36c6cd1ac3541aa3422b62facc55ad598564
Desarrollojunin <desarrolloswebjunin@gmail.com>
2022-07-12 16:00:54 -0300

    curos de php

---
+++ assets/index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Blog de Gisela Nievas</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+    </head>
+    <body>
+        <header id="cabecera">
+
+            <div id="logo">
+                <a href="index.php">
+                   Listado
+                </a>   
+            </div>
+            <div class="clearfix"></div>
+        </header>
+
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="index.php">Categoria 1</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="index.php">Categoria 2</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="index.php">Categoria 3</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="index.php">Categoria 4</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="index.php">Sobre mí</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="index.php">Contacto</a>
+                </li>
+            </ul>
+
+        </nav>
+
+    
+        
+        <div id="contenedor">
+            <aside id="sidebar">
+                <div id="login" class="bloque">
+                    <h3>Registrate</h3>
+                    <form action="login.php" method="POST">
+                        <label for="nombre">Email</label>
+                        <input type="text" name="email"/>
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password"/>
+                        <input type="submit" value="Entrar">
+                    </form>
+                </div>
+
+            </aside>
+        </div>
+        <div id="contenedor">
+            <aside id="sidebar">
+                <div id="login" class="bloque">
+                    <h3>Identificate</h3>
+                    <form action="login.php" method="POST">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" name="nombre"/>
+                        <label for="apellido">Apellidos</label>
+                        <input type="text" name="apellidos"/>
+                        <label for="apellido">Email</label>
+                        <input type="text" name="apellidos"/>
+                        <label for="apellido">Contraseña</label>
+                        <input type="text" name="apellidos"/>
+                        <input type="submit" value="Entrar">
+                    </form>
+                </div>
+
+            </aside>
+        </div>
+      
+     
+        <div id="principal">
+        <div>
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Descripción de la entrada
+                </p>
+            </article>
+        </div>
+        <div>
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Descripción de la entrada
+                </p>
+            </article>
+        </div>
+        <div >
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Descripción de la entrada
+                </p>
+            </article>
+        </div>
+        <div>
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Descripción de la entrada
+                </p>
+            </article>
+        </div>
+        <div>
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Descripción de la entrada
+                </p>
+            </article>
+        </div>
+        <div>
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Descripción de la entrada
+                </p>
+            </article>
+        </div>
+        <div>
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Descripción de la entrada
+                </p>
+            </article>
+        </div>
+     
+
+        <div class="clearfix"></div>
+    
+       
+       </div>
+       
+       
+       
+       
+       
+        <footer id="pie">
+            <p>Desarrollado por Gisela Nievas</p>
+
+        </footer>
+
+   </body>
+
+</html>
